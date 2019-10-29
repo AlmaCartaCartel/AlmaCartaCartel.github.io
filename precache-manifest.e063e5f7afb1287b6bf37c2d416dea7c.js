11b66545a5ba69c9af38b6c3087755c8346e360e
AlmaCartaCartel <karitanjecksparov@gmail.com>
2019-10-29 20:26:30 +0300

    build

---
+++ precache-manifest.e063e5f7afb1287b6bf37c2d416dea7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a56ed77618e432eeede05ee6d94b9b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3277c26ceefca638ff",
+    "url": "/static/css/main.14e1049c.chunk.css"
+  },
+  {
+    "revision": "979ea5644d96933380a7",
+    "url": "/static/js/2.82c1956b.chunk.js"
+  },
+  {
+    "revision": "3b3277c26ceefca638ff",
+    "url": "/static/js/main.1484800c.chunk.js"
+  },
+  {
+    "revision": "d8e35029ba6b9de5f2ee",
+    "url": "/static/js/runtime-main.9450b992.js"
+  },
+  {
+    "revision": "81c984809a7211324bc1ac853fcd8a18",
+    "url": "/static/media/deth-star.81c98480.png"
+  }
+]);
